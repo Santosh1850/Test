962c52f42a8d69402878f3e7565a5e0f35ad7c51
Santosh1850 <santoshkirdak022@gmail.com>
2025-04-02 11:42:52 +0530

    New Changes

---
+++ demo2.java
@@ -0,0 +1,11 @@
+public class demo2{
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    public void payment(){
+        System.out.println("M1");
+    }
+    public void notification(){
+        System.out.println("Hello uesr");
+    }
+}
